0ee1fa432623c0728b45d90af5edface95ad16c5
Martin Senécal <martinsenecal2000@gmail.com>
2020-06-12 17:51:44 -0400

    Add Test for Sequence

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Driver {
     public static void main(String[] args) {
         AVL tree = new AVL();
@@ -13,5 +15,24 @@ public class Driver {
         tree.remove("AAMNF3INWW");
         System.out.println();
         tree.allKeys();
+        System.out.println("==================================");
+
+        Sequence sq=new Sequence();
+        sq.add("A294V62IFM2", new Vehicle("A294V62IFM2"));
+        sq.add("E2JG90PA5F", new Vehicle("E2JG90PA5F"));
+        sq.add("AAMNfF3INWW", new Vehicle("AAMNfF3INWW"));
+        sq.add("FEN39FS20P", new Vehicle("FEN39FS20P"));
+        sq.add("IYN28FB25E", new Vehicle("IYN28FB25E"));
+
+        ArrayList<String> list=sq.allKeys();
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        sq.remove("A294V62IFM2");
+        System.out.println();
+        ArrayList<String> list2=sq.allKeys();
+        for(int i=0;i<list2.size();i++){
+            System.out.println(list2.get(i));
+        }
     }
 }
